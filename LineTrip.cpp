5b1a91d4fef125665180d603fb69aebccf16b1d1
heyitsmeravi <heyitsmeravi01@gmail.com>
2025-04-26 16:05:13 +0530

    solved the 2nd problem

--- LineTrip.cpp
+++ LineTrip.cpp
@@ -12,8 +12,16 @@ int main(){
         for (int i=0;i<n;i++){
             cin>>a[i];
         }
+        vector<int>fuels=a;
+        fuels.push_back(x);
+        fuels.insert(fuels.begin(),0);
         int maxVol=0;
-        for (   )
+        for (int i=0;i<fuels.size()-2;i++){
+            maxVol=max((fuels[i+1]-fuels[i]),maxVol);
+        }
+        maxVol=max(maxVol,2*(fuels[n+1]-fuels[n]));
+        cout<<maxVol<<endl;
+
     }
     return 0;
 }
