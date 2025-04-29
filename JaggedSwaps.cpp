c6671836e177b1b0de00563d5e4aee649eb97374
heyitsmeravi <heyitsmeravi01@gmail.com>
2025-04-29 14:46:35 +0530

    fifth problem solved

---
+++ JaggedSwaps.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while (t--){
+        int n;
+        cin>>n;
+        vector <int> permutation(n);
+        for (int i=0;i<n;i++){
+            cin>>permutation[i];
+        }
+        if (permutation[0]==1){
+            cout<<"YES"<<endl;
+        }else{
+            cout<<"NO"<<endl;
+        }
+
+    }
+    return 0;
+}
